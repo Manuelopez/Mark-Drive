d1dba996eec90a2dcef815859c2a6d2fb61e3395
Manuel <manuelopez18@hotmail.com>
2020-05-24 02:39:59 -0400

    handle mongo db connection error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  },
+  (err, db) => {
+    if (err) {
+      console.log('Error could not connect to server');
+    } else {
+      console.log('connect correctly');
+    }
+  }
+);
